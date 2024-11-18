ad406dc539f767bdd7035e77fbddbaab06a4490a
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-18 10:37:41 +0330

    feat: react query helper types

--- src/lib/react-query/index.ts
+++ src/lib/react-query/index.ts
@@ -1,4 +1,4 @@
-import { DefaultOptions } from '@tanstack/react-query';
+import { DefaultOptions, UseMutationOptions } from '@tanstack/react-query';
 
 export const queryConfig = {
   queries: {
@@ -7,3 +7,19 @@ export const queryConfig = {
     refetchOnWindowFocus: false,
   },
 } satisfies DefaultOptions;
+
+export type ApiFnReturnType<FnType extends (...args: any) => Promise<any>> =
+  Awaited<ReturnType<FnType>>;
+
+export type QueryConfig<T extends (...args: any[]) => any> = Omit<
+  ReturnType<T>,
+  'queryKey' | 'queryFn'
+>;
+
+export type MutationConfig<
+  MutationFnType extends (...args: any[]) => Promise<any>,
+> = UseMutationOptions<
+  ApiFnReturnType<MutationFnType>,
+  Error,
+  Parameters<MutationFnType>[0]
+>;
