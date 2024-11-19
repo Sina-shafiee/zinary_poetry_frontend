fbd38929e5495c6e583373d1658a9093ce7da4cf
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-19 14:47:09 +0330

    fix: minor improvement

--- src/components/layout/writer-dashboard-sidebar.tsx
+++ src/components/layout/writer-dashboard-sidebar.tsx
@@ -1,21 +1,34 @@
+import { Link } from 'react-router-dom';
 import {
   Settings,
   LayoutDashboard,
   UserRound,
   Blocks,
   Book,
+  ChevronUp,
 } from 'lucide-react';
+
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from '../ui/sidebar';
 import { Separator } from '../ui/separator';
-import { Link } from 'react-router-dom';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+
+import { useUser } from '@/features/auth/api/get-user';
+
 import { paths } from '@/config/paths';
+import { useLogout } from '@/features/auth/api/logout';
 
 const links = [
   {
@@ -45,16 +58,28 @@ const links = [
   },
 ];
 const WriterDashboardSidebar = () => {
+  const user = useUser({});
+  const logout = useLogout({});
+
   return (
-    <Sidebar side="right" variant="floating">
+    <Sidebar collapsible="offcanvas" side="right" variant="floating">
       <SidebarHeader>
-        <h2 className="px-2 font-semibold">پنل نویسندگان</h2>
+        <div className="flex items-center mt-2">
+          <div className="size-8 ms-2">
+            <img
+              className="size-8"
+              src="https://zinary.net/logo.png"
+              alt="logo"
+            />
+          </div>
+          <h2 className="px-2 font-semibold">پنل نویسندگان</h2>
+        </div>
       </SidebarHeader>
       <Separator className="mt-2 mx-2 w-[calc(100%-1rem)]" />
       <SidebarContent>
         <SidebarMenu className="pt-1">
           {links.map(item => (
-            <SidebarMenuItem className="mt-1" key={item.title}>
+            <SidebarMenuItem key={item.title}>
               <SidebarMenuButton asChild>
                 <Link to={item.url}>
                   <item.icon strokeWidth={1.4} />
@@ -65,6 +90,37 @@ const WriterDashboardSidebar = () => {
           ))}
         </SidebarMenu>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <div>{user.data!.data.email}</div>
+                  <ChevronUp className="ms-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width] bg-card text-card-foreground border border-input shadow-sm"
+              >
+                <DropdownMenuItem
+                  className="focus:bg-background focus:text-foreground cursor-pointer"
+                  asChild
+                >
+                  <SidebarMenuButton
+                    onClick={() => {
+                      logout.mutate(undefined, {});
+                    }}
+                  >
+                    <span>خروج از پنل</span>
+                  </SidebarMenuButton>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   );
 };
