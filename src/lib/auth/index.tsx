113d2a99d414006a706187efd095c27099d3014b
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-19 09:53:09 +0330

    fix: redirect url

--- src/lib/auth/index.tsx
+++ src/lib/auth/index.tsx
@@ -1,3 +1,4 @@
+import { paths } from '@/config/paths';
 import { useUser } from '@/features/auth/api/get-user';
 import { Navigate } from 'react-router-dom';
 
@@ -5,8 +6,9 @@ export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const user = useUser({});
 
   if (!user.data) {
-    // TODO: read login path from config.paths
-    return <Navigate to={'/login'} replace />;
+    return (
+      <Navigate to={paths.auth.login.getHref(location.pathname)} replace />
+    );
   }
 
   return children;
