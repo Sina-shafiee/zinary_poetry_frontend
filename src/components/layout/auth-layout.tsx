ee50e1c4b1085d123fd417e3eb08fbbff2843bf1
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-19 10:29:23 +0330

    fix: added editional props

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -3,13 +3,19 @@ import { Navigate, useSearchParams } from 'react-router-dom';
 
 import { useUser } from '@/features/auth/api/get-user';
 import { paths } from '@/config/paths';
+import { cn } from '@/lib/utils';
 
 interface Props {
   title: string;
   children: React.ReactNode;
+  classNames?: {
+    container?: string;
+    formContainer?: string;
+    title?: string;
+  };
 }
 
-export const AuthLayout = ({ title, children }: Props) => {
+export const AuthLayout = ({ title, children, classNames }: Props) => {
   const user = useUser({});
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get('redirectTo');
@@ -21,9 +27,26 @@ export const AuthLayout = ({ title, children }: Props) => {
   }
 
   return (
-    <main className="flex min-h-dvh items-center justify-center bg-background">
-      <section className="max-w-md sm:max-w-sm rounded p-6 w-full bg-background sm:bg-card relative z-20 space-y-6 sm:shadow-sm">
-        <h2 className="text-xl mt-4 font-semibold sm:text-2xl">{title}</h2>
+    <main
+      className={cn(
+        'flex min-h-dvh items-center justify-center bg-background',
+        classNames?.container,
+      )}
+    >
+      <section
+        className={cn(
+          'max-w-md sm:max-w-sm rounded p-6 w-full bg-background sm:bg-card relative z-20 space-y-6 sm:shadow-sm',
+          classNames?.formContainer,
+        )}
+      >
+        <h2
+          className={cn(
+            'text-xl mt-4 font-semibold sm:text-2xl',
+            classNames?.title,
+          )}
+        >
+          {title}
+        </h2>
         {children}
       </section>
     </main>
