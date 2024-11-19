f3f101e4a48d4d84084d4badcb4f64844b2dda74
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-19 10:02:33 +0330

    feat(api): error handling

--- src/lib/api/rest-api.ts
+++ src/lib/api/rest-api.ts
@@ -1,6 +1,7 @@
-import Axios, { InternalAxiosRequestConfig } from 'axios';
+import Axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 import { env } from '@/config/env';
+import { toast } from 'sonner';
 
 export const baseApi = Axios.create({
   baseURL: env.REST_API_URL,
@@ -23,7 +24,21 @@ baseApi.interceptors.response.use(
   response => {
     return response;
   },
-  error => {
+  (error: unknown) => {
+    if (error instanceof AxiosError) {
+      const { response, status } = error;
+
+      if (
+        status === 400 &&
+        response?.data?.code_name === 'bad_request' &&
+        typeof response.data.details === 'string'
+      ) {
+        toast.error(response.data.details);
+      } else if (error.code === 'ERR_NETWORK') {
+        toast.error('خطایی در برقراری ارتباط با سرور رخ داد');
+      }
+    }
+
     return Promise.reject(error);
   },
 );
