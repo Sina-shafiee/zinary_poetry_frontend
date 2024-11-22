ecb827c4993af96984f1654918e83a71812e6a54
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-22 22:56:29 +0330

    feat(ui): date picker component

---
+++ src/components/ui/date-picker/date-picker.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import MultiDatePicker from 'react-multi-date-picker';
+import persian from 'react-date-object/calendars/persian';
+import persian_fa from 'react-date-object/locales/persian_fa';
+
+const DatePicker = React.forwardRef<
+  React.ElementRef<typeof MultiDatePicker>,
+  React.ComponentPropsWithoutRef<typeof MultiDatePicker>
+>((props, ref) => {
+  return (
+    <MultiDatePicker
+      ref={ref as any}
+      format="YYYY/MM/DD"
+      calendar={persian}
+      locale={persian_fa}
+      arrowClassName="after:!shadow-sm"
+      containerClassName="w-full h-10 bg-transparent [&_rmdp-wrapper]:!shadow-none !shadow-sm rounded-md mt-1 flex h-9 w-full rounded-md border border-input"
+      inputClass="rounded-md px-2 px-3 py-1 text-base !shadow-none transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium bg-transparent file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+      className="mt-1 rounded-md w-full"
+      calendarPosition="bottom-right"
+      style={{
+        height: '40px',
+        minHeight: '40px',
+        width: '100%',
+      }}
+      arrowStyle={{
+        boxShadow: 'none',
+      }}
+      {...props}
+    />
+  );
+});
+
+DatePicker.displayName = 'DatePicker';
+
+export { DatePicker };
