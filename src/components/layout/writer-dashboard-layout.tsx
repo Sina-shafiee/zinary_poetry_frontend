3e111c0d80a1c0e58f214f857728ac23e547ec90
Sina Shafiee <alisinashafieezada@gmail.com>
2024-11-22 08:32:35 +0330

    fix: padding xd

--- src/components/layout/writer-dashboard-layout.tsx
+++ src/components/layout/writer-dashboard-layout.tsx
@@ -3,18 +3,28 @@ import { Outlet } from 'react-router-dom';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 
 import WriterDashboardSidebar from './writer-dashboard-sidebar';
+import { useSidebar } from '../ui/sidebar/user-sidebar';
+import { cn } from '@/lib/utils';
+
+const MainContent = () => {
+  const { open } = useSidebar();
+
+  return (
+    <main className={cn('w-full p-2 pb-12 ', open && 'lg:pe-4')}>
+      <div className="h-20 rounded shadow bg-card flex items-center ps-4 gap-2">
+        <SidebarTrigger />
+        <p>هدر وبسایت اینجا قرار خواهد گرفت D:</p>
+      </div>
+      <Outlet />
+    </main>
+  );
+};
 
 const WriterDashboardLayout = () => {
   return (
     <SidebarProvider>
       <WriterDashboardSidebar />
-      <main className="w-full p-2 pb-12 lg:pe-4">
-        <div className="h-20 rounded shadow bg-card flex items-center ps-4 gap-2">
-          <SidebarTrigger />
-          <p>هدر وبسایت اینجا قرار خواهد گرفت D:</p>
-        </div>
-        <Outlet />
-      </main>
+      <MainContent />
     </SidebarProvider>
   );
 };
